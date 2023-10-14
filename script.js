62cfdf92b79b4ee36aae958458d6d0e5f9c69aa6
Ahmed Ramadan <143494954+ahmediramadan01@users.noreply.github.com>
2023-10-14 18:35:47 +0300

    Fix empty marker game boar UI bug

--- script.js
+++ script.js
@@ -3,18 +3,27 @@
 const gameBoard = (function () {
     const gameBoardElement = document.querySelector(".game-board");
 
-    const board = ["o", "o", "o", "o", "o", "o", "o", "o", "o"];
+    const board = ["", "", "", "", "x", "", "o", "", ""];
+
+    const getBoard = () => board;
 
     const render = function () {
         gameBoardElement.innerHTML = "";
 
-        board.forEach((marker) =>
-            gameBoardElement.insertAdjacentHTML(
-                "beforeend",
-                `<div class="cell"><img src="./images/${marker}.svg" alt="" /></div>`
-            )
-        );
+        board.forEach((marker, index) => {
+            if (marker !== "") {
+                gameBoardElement.insertAdjacentHTML(
+                    "beforeend",
+                    `<div class="cell" data-cell="${index}"><img src="./images/${marker}.svg" alt="" /></div>`
+                );
+            } else {
+                gameBoardElement.insertAdjacentHTML("beforeend", `<div class="cell" data-cell="${index}"></div>`);
+            }
+        });
     };
 
-    return { render };
+    return { getBoard, render };
 })();
+
+gameBoard.render();
+console.log(gameBoard.getBoard());
