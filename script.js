cd615aae3249b188140584885eac35acbe75f910
Ahmed Ramadan <143494954+ahmediramadan01@users.noreply.github.com>
2023-10-14 22:22:41 +0300

    Implement game controller module

--- script.js
+++ script.js
@@ -3,27 +3,134 @@
 const gameBoard = (function () {
     const gameBoardElement = document.querySelector(".game-board");
 
-    const board = ["", "", "", "", "x", "", "o", "", ""];
+    const board = ["", "", "", "", "", "", "", "", ""];
+
+    const getGameBoardElement = () => gameBoardElement;
 
     const getBoard = () => board;
 
-    const render = function () {
-        gameBoardElement.innerHTML = "";
+    const modifyBoard = function (position, marker) {
+        board[position] = marker;
+    };
 
-        board.forEach((marker, index) => {
-            if (marker !== "") {
-                gameBoardElement.insertAdjacentHTML(
-                    "beforeend",
-                    `<div class="cell" data-cell="${index}"><img src="./images/${marker}.svg" alt="" /></div>`
-                );
-            } else {
-                gameBoardElement.insertAdjacentHTML("beforeend", `<div class="cell" data-cell="${index}"></div>`);
-            }
+    const renderCell = function (position) {
+        const cell = gameBoardElement.querySelector(`[data-cell="${position}"]`);
+        if (board[position] !== "") {
+            cell.innerHTML = `<img src="./images/${board[position]}.svg" alt="" />`;
+        } else {
+            cell.innerHTML = "";
+        }
+    };
+
+    const clearBoard = function () {
+        gameBoardElement.innerHTML = "";
+        board.forEach((_, index) => {
+            gameBoardElement.insertAdjacentHTML("beforeend", `<div class="cell" data-cell="${index}"></div>`);
         });
     };
 
-    return { getBoard, render };
+    return { getGameBoardElement, getBoard, modifyBoard, clearBoard, renderCell };
 })();
 
-gameBoard.render();
-console.log(gameBoard.getBoard());
+gameBoard.clearBoard();
+
+const Player = function (name, marker) {
+    const playerMove = function (position) {
+        if (position >= 0 && position < 9) {
+            gameBoard.modifyBoard(position, marker);
+        }
+    };
+    return { name, marker, playerMove };
+};
+
+const gameController = (function () {
+    const playerX = Player("playerX", "x");
+    const playerO = Player("playerO", "o");
+
+    let currentPlayer = playerX,
+        gameOver = false,
+        winner;
+
+    const announcementElement = document.querySelector(".announcement");
+
+    const playGame = function () {
+        const checkForWin = function () {
+            const winningCombinations = [
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8],
+                [0, 3, 6],
+                [1, 4, 7],
+                [2, 5, 8],
+                [0, 4, 8],
+                [2, 4, 6],
+            ];
+
+            for (const combination of winningCombinations) {
+                if (
+                    gameBoard.getBoard()[combination[0]] !== "" &&
+                    gameBoard.getBoard()[combination[0]] === gameBoard.getBoard()[combination[1]] &&
+                    gameBoard.getBoard()[combination[0]] === gameBoard.getBoard()[combination[2]]
+                ) {
+                    return true;
+                }
+            }
+            return false;
+        };
+
+        const checkForTie = function () {
+            for (const marker of gameBoard.getBoard()) {
+                if (marker === "") {
+                    return false;
+                }
+            }
+
+            return !checkForWin();
+        };
+
+        const handlePlayerMove = function (event) {
+            if (!gameOver) {
+                gameBoard.modifyBoard(event.target.dataset.cell, currentPlayer.marker);
+
+                gameBoard.renderCell(event.target.dataset.cell);
+
+                if (checkForWin()) {
+                    gameOver = true;
+                    winner = currentPlayer;
+                    announcementElement.textContent = winner === playerX ? "Player X WON!" : "Player O WON!";
+                    return;
+                } else if (checkForTie()) {
+                    gameOver = true;
+                    announcementElement.textContent = "TIE!";
+                    return;
+                }
+
+                currentPlayer = currentPlayer === playerX ? playerO : playerX;
+                announcementElement.textContent = currentPlayer === playerX ? "Player X's turn" : "Player O's turn";
+            }
+        };
+
+        const restartGame = function (event) {
+            if (event.key === "r") {
+                for (let i = 0; i < 9; i++) {
+                    gameBoard.modifyBoard(i, "");
+                }
+                gameBoard.clearBoard();
+                currentPlayer = playerX;
+                announcementElement.textContent = currentPlayer === playerX ? "Player X's turn" : "Player O's turn";
+            }
+        };
+
+        gameBoard.getGameBoardElement().addEventListener("click", function (event) {
+            if (event.target.classList.contains("cell")) {
+                handlePlayerMove(event);
+            }
+        });
+
+        window.addEventListener("keydown", restartGame);
+    };
+
+    return { playGame };
+})();
+
+gameController.playGame();
